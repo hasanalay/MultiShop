940a09d846af187bdf8f4b607471d989bbd01f4c
hasanalay <hasan.alay@outlook.com>
2024-04-28 16:53:59 +0300

    create and implement discount microservice and create order microservice

---
+++ Services/Discount/MultiShop.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Discount.Dtos;
+using MultiShop.Discount.Services;
+
+namespace MultiShop.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+
+        public DiscountsController(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DiscountCouponList()
+        {
+            var values = await _discountService.GetAllDiscountCouponsAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDiscountCouponById(int id)
+        {
+            var values = await _discountService.GetByIdDiscountCouponAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDiscountCoupon(CreateDiscountCouponDto createDiscountCouponDto)
+        {
+            await _discountService.CreateDiscountCouponAsync(createDiscountCouponDto);
+            return Ok("The Coupon was created successfully!");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDiscountCoupon(int id)
+        {
+            await _discountService.DeleteDiscountCouponAsync(id);
+            return Ok("The Coupon was deleted successfully!");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateDiscountCoupon(UpdateDiscountCouponDto updateDiscountCouponDto)
+        {
+            await _discountService.UpdateDiscountCouponAsync(updateDiscountCouponDto);
+            return Ok("The Coupon was updated successfully!");
+        }
+    }
+}
